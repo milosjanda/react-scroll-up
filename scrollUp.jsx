1ea09cb6281dd26a8050ec8e94fa1102f2f4362c
Gregory Benner <gregbenner1@gmail.com>
2016-10-04 16:22:00 -0400

    removed console.log that I somehow managed to commit...

--- scrollUp.jsx
+++ scrollUp.jsx
@@ -69,7 +69,6 @@ var ScrollUp = React.createClass({
     },
 
     handleScroll: function () {
-      console.log('got here')
         if (window.pageYOffset > this.props.showUnder) {
             this.setState({show: true});
         } else {
